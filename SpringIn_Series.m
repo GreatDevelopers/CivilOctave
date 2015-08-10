43c3735ae1f05ca73398defaeb4fbd5561aa6c64
hsrai <hardeep.rai@gmail.com>
2015-08-10 12:31:17 +0530

    Variable name changed

--- SpringIn_Series.m
+++ SpringIn_Series.m
@@ -2,52 +2,58 @@
 clc
 clear
 
-%% Initialization [PRE-PROCESSING]
+%% Initialisation [PRE-PROCESSING]
 
-% nn: Number of nodes
-% ne: Number of Elements
-% K: Global stiffness matrix
-% k: Local stiffness matrix
-% P: Load matrix
-% nc: Nodal matrix (Information on nodes correspinding to each elements)
+% Follwing are the main variables used. No explaination is given as
+%	names are self explanatory.
+% Number_of_nodes nn
+% Number_of_Elements ne
+% Global_stiffness_matrix K
+% Local_stiffness_matrix k
+% Load_matrix P
+% Element_Incidences nc: Nodal matrix
+%	(Information on nodes correspinding to each elements)
 
 
-nn=5; % Enter number of nodes
-ne=4; % Enter number of elements
+Number_of_nodes = 5; % Enter number of nodes
+Number_of_Elements = 4; % Enter number of elements
 
-K(nn,nn)=0; %Variable initialization
-F(nn,1)=0;  % -same- 
+% Node numbers for each element in row form
+Element_Incidences = [1 2; 2 3; 3 4; 4 5]
 
-%Node numbers for each element in row form
-nc=[1 2; 2 3; 3 4; 4 5]
-
-% Spring constants in column form
-k=[100 100 100 100 ]'
+% k: Spring constants in column form
+Local_stiffness_matrix = [100 100 100 100 ]'
 
 %Load matrix in column form
-P=[0 0 -20 -30 30]'
+Load_matrix = [0 0 -20 -30 30]'
+
+%Variable initialisation
+Global_stiffness_matrix(Number_of_nodes,Number_of_nodes) = 0;
+% F(Number_of_nodes,1) = 0; % *** rm it ***
 
 
 %% [PROCESSING]
 
 
-%Following loop adds local matrices of each element to form Global
-%Matrix (K), and F the global force matrix
+% Following loop adds local matrices of each element to form Global
+% Matrix.
 
-for e=1:ne
+%, and F the global force matrix  *** rm it ***
+ 
+for element_i = 1:Number_of_Elements
     
-    i=nc(e,1);
-    j=nc(e,2);
-    ks=k(e)*[1 -1;-1 1];
+    i = Element_Incidences(element_i,1);
+    j = Element_Incidences(element_i,2);
+    ks = Local_stiffness_matrix(element_i)*[1 -1;-1 1];
     
-    K(i,i)=ks(1,1)+K(i,i);
-    K(i,j)=ks(1,2)+K(i,j);
-    K(j,i)=ks(2,1)+K(j,i);
-    K(j,j)=ks(2,2)+K(j,j);
+    Global_stiffness_matrix(i,i) = ks(1,1) + Global_stiffness_matrix(i,i);
+    Global_stiffness_matrix(i,j) = ks(1,2) + Global_stiffness_matrix(i,j);
+    Global_stiffness_matrix(j,i) = ks(2,1) + Global_stiffness_matrix(j,i);
+    Global_stiffness_matrix(j,j) = ks(2,2) + Global_stiffness_matrix(j,j);
 
 end
 
 %% [POST-PROCESSING]
 
-K % Global stiffness matrix
-P % GLobal force vector
+Global_stiffness_matrix % Global stiffness matrix
+Load_matrix % GLobal force vector
