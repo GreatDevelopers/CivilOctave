448504d11865964bc380f5e47ce53bb18dddc725
hsrai <hardeep.rai@gmail.com>
2015-08-25 04:51:34 +0530

    Formatting changes

--- Examples/DoS/01/main.m
+++ Examples/DoS/01/main.m
@@ -5,11 +5,11 @@ clear
 %% Initialisation [PRE-PROCESSING]
 
 % Follwing are the main variables used. No explaination is given as
-%	names are self explanatory.
+%  names are self explanatory.
 % Number_of_storeys
 % Mass
 % Stiffness_story
-%	(Summed shear stiffness of all columns in a story)
+%  (Summed shear stiffness of all columns in a story)
 
 
 load input.mat
@@ -34,59 +34,59 @@ Stiffness_story
 
 for storey_i = 1:Number_of_storeys
 
-	Stiffness_matrix(storey_i, storey_i) = ...
-		Stiffness_story(storey_i);
+  Stiffness_matrix(storey_i, storey_i) = ...
+    Stiffness_story(storey_i);
 
-	if (storey_i < Number_of_storeys )
-		Stiffness_matrix(storey_i, storey_i) = ...
-			Stiffness_matrix(storey_i, storey_i) + ...
-			Stiffness_story(storey_i + 1);
-		Stiffness_matrix(storey_i, storey_i + 1) = ...
-			- Stiffness_story(storey_i + 1);
+  if (storey_i < Number_of_storeys )
+    Stiffness_matrix(storey_i, storey_i) = ...
+      Stiffness_matrix(storey_i, storey_i) + ...
+      Stiffness_story(storey_i + 1);
+    Stiffness_matrix(storey_i, storey_i + 1) = ...
+      - Stiffness_story(storey_i + 1);
                 Stiffness_matrix(storey_i + 1, storey_i) = ...
-			Stiffness_matrix(storey_i, storey_i + 1);
- 	endif
+      Stiffness_matrix(storey_i, storey_i + 1);
+   endif
     
 end
 
-for storey_i = 1:Number_of_storeys
+for storey_i = 1 : Number_of_storeys
 
-	Level_floor(storey_i, 1) = ...
-		Height_storey(storey_i,1);
+  Level_floor(storey_i, 1) = ...
+    Height_storey(storey_i,1);
 
-	if (storey_i > 1 )
-		 Level_floor(storey_i, 1) = ...
-			Level_floor(storey_i, 1) + ...
-			  Level_floor(storey_i - 1, 1);
- 	endif
+  if (storey_i > 1 )
+     Level_floor(storey_i, 1) = ...
+      Level_floor(storey_i, 1) + ...
+        Level_floor(storey_i - 1, 1);
+   endif
     
 end
 
 [Eigen_vector, Omega_square] = eig(Stiffness_matrix, Mass);
 Omega = sqrt(Omega_square);
-for storey_i = 1:Number_of_storeys
+for storey_i = 1 : Number_of_storeys
   Time_period(storey_i, storey_i) = 2 * pi() ...
     / sqrt(Omega_square(storey_i, storey_i)); 
 end
 
-for storey_i = 1:Number_of_storeys
+for storey_i = 1 : Number_of_storeys
  Frequency(storey_i,1) = Omega(storey_i, storey_i);
 end
 
-for storey_i = 1:Number_of_storeys
+for storey_i = 1 : Number_of_storeys
  Time_periods(storey_i,1) = Time_period(storey_i, storey_i);
 end
 
-for index_k = 1:Number_of_storeys
-	sum_W_Phi = 0;
-	sum_W_Phi2 = 0;
-	for index_i = 1:Number_of_storeys
-		sum_W_Phi = sum_W_Phi + Mass(index_i, index_i) * ...
-			Eigen_vector(index_i, index_k);
-		sum_W_Phi2 = sum_W_Phi2 + Mass(index_i, index_i) * ...
-			Eigen_vector(index_i, index_k)^2;
-	end
-	Modal_participation_factor(index_k,1) = sum_W_Phi / sum_W_Phi2;
+for index_k = 1 : Number_of_storeys
+  sum_W_Phi = 0;
+  sum_W_Phi2 = 0;
+  for index_i = 1 : Number_of_storeys
+    sum_W_Phi = sum_W_Phi + Mass(index_i, index_i) * ...
+      Eigen_vector(index_i, index_k);
+    sum_W_Phi2 = sum_W_Phi2 + Mass(index_i, index_i) * ...
+      Eigen_vector(index_i, index_k)^2;
+  end
+  Modal_participation_factor(index_k,1) = sum_W_Phi / sum_W_Phi2;
 end
 
 %% [POST-PROCESSING]
@@ -111,7 +111,7 @@ Modal_participation_factor
 
 %% Plot mode shapes
 
-plotHangle = figure('visible','off')
+plotHangle = figure('visible', 'off')
 
 plot([0; Eigen_vector(:,1)], [0; Level_floor],'-ro')
 hold on
