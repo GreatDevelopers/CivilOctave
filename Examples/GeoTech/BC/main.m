cb6dee58b8973a1aa64bf0416fefe6c9cb82e168
hsrai <hardeep.rai@gmail.com>
2015-09-09 21:34:32 +0530

    Initial commit

---
+++ Examples/GeoTech/BC/main.m
@@ -0,0 +1,21 @@
+% Program to claculate Bearing Capacity
+clc
+clear
+
+%% Initialisation [PRE-PROCESSING]
+
+% Following are the main variables used. No explanation is given as
+%	names are self explanatory.
+%
+
+load input.mat
+load dataFile.mat
+
+%Variable initialisation
+%Global_stiffness_matrix(Number_of_nodes,Number_of_nodes) = 0;
+
+%% [PROCESSING]
+
+
+
+% End of file
