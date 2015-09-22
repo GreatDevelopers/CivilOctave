a58814a572290e2b29d6e153dde2c2979104e830
amarjeetkapoor1 <amarjeet.kapoor1@gmail.com>
2015-09-22 19:51:30 +0530

    pdf edited

--- CivilSage/cgi/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-print("Content-Type: text/html")
-print()
-import cgi,cgitb
-cgitb.enable()
-form = cgi.FieldStorage()
-seachterm =  form.getvalue('searchbox')
-print("Name of the user is:", searchterm)
