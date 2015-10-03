36e3d10ecbf06ccbf1ace995ddc94a795c64e940
mandeeps708 <mandeeps708@gmail.com>
2015-10-03 11:32:14 +0530

    bootstrap working

--- sage/sage/settings.py
+++ sage/sage/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -26,7 +26,11 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+#  Templates location
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "/sage/templates"),
+)
+print("Template-dirs: ",TEMPLATE_DIRS)
 # Application definition
 
 INSTALLED_APPS = (
@@ -67,7 +71,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -75,10 +79,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR), "sage/static/"),
+)
+print('Staticfiles-dirs: ',STATICFILES_DIRS)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
