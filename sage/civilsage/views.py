7146c846db288e648f95e9b9817f889d81001166
amarjeetkapoor1 <amarjeet.kapoor1@gmail.com>
2015-10-16 22:18:04 +0530

    minor changes in file function and templetes

--- sage/civilsage/views.py
+++ sage/civilsage/views.py
@@ -164,7 +164,8 @@ def last(request):
 			os.system(command)
 			return response
 	except:
-		return render(request, "civilsage/matrix.html",{'message':message,'email_get':request.session.get('email_get')})
+		return render(request, "civilsage/matrix.html",
+		{'message':message,'email_get':request.session.get('email_get')})
 
 
 """Documentation for a function
@@ -174,105 +175,113 @@ to give output in form of response
 def file(request):
 
 	message='please fill again'
-	#dictionary of all input tags
-	lists = {'Soil_type':'','Number_of_storeys':''
-	,'Importance_factor':'','Response_reduction_factor':''
-	,'Zone_factor':'','Gravity_acceleration':''
-	,'Modes_considered':''}
-	#name of directory of specific user
-	name=''
+	try:
+		#dictionary of all input tags
+		lists = {'Soil_type':'','Number_of_storeys':''
+		,'Importance_factor':'','Response_reduction_factor':''
+		,'Zone_factor':'','Gravity_acceleration':''
+		,'Modes_considered':''}
+		#name of directory of specific user
+		name=''
 
-	#getting input using tags
-	for var in lists.keys():
-		lists[var]=request.session.get(var)
-		name=name+str(lists[var])
+		#getting input using tags
+		for var in lists.keys():
+			lists[var]=request.session.get(var)
+			name=name+str(lists[var])
 
-	command='cp -r sagemath '+name
-	os.popen(command)
+		command='cp -r sagemath '+name
+		os.popen(command)
 
-	#opening file for writing
-	command=name+'/input.sage'
-	file = open(command, 'w')
+		#opening file for writing
+		command=name+'/input.sage'
+		file = open(command, 'w')
 
-	#writing variables in input.sage file with syntax of sage
-	for var in lists.keys():
-		file.write(var)
-		file.write('=')
-		file.write(lists[var])
-		file.write('\n')
-	file.close()
+		#writing variables in input.sage file with syntax of sage
+		for var in lists.keys():
+			file.write(var)
+			file.write('=')
+			file.write(lists[var])
+			file.write('\n')
+		file.close()
 
-	#getting file uploaded by user
-	f=request.FILES["input_file"]
-	if(f.content_type != 'text/plain'):
-		print(request.session.get('email_get'))
-		return render( request,'civilsage/file.html',
-		{'email_get': request.session.get('email_get'),
-		'message':'File Not in CSV FORMAT '})
-	data = [row for row in csv.reader(f)]
-	#getting numbers of storeys
-	num = request.session.get('Number_of_storeys')
+		#getting file uploaded by user
+		f=request.FILES["input_file"]
+		print(f.content_type)
+		if(f.content_type != 'text/csv'):
+			return render( request,'civilsage/file.html',
+			{'email_get': request.session.get('email_get'),
+			'message':'File Not in CSV FORMAT '})
+		data = [row for row in csv.reader(f)]
+		#getting numbers of storeys
+		num = request.session.get('Number_of_storeys')
 
-	#checking if number values are not less than required values
-	#if(len(data)<3*int(num)):
-		#return render( request,'civilsage/file.html')
-	#opening input.sage to append remaining inputs
-	command=name+'/input.sage'
-	file=open(command,'a')
-	#list of basic tags
-	var = ['mass','Height_storey','Stiffness_storey']
-	jar=0
-	#writing matix into sage file
-	for j in var:
-		message="Less no. of rows in csv file"
-		file.write(j)
-		file.write('=matrix([')
-		#writing elements of matix
-		for i in range(int(num)):
-			file.write('[')
-			#getting input from tags
-			message="Less no. of elements in row "+str(i)
-			d=data[jar][i]
-			print(d)
-			file.write(str(d))
-			file.write(']')
-			#condition to check last element
-			if( i!=int(num)+jar-1):
-				file.write(',')
-		jar=jar+1
-		file.write('])\n')
-	file.close()
+		#checking if number values are not less than required values
+		#if(len(data)<3*int(num)):
+			#return render( request,'civilsage/file.html')
+		#opening input.sage to append remaining inputs
+		command=name+'/input.sage'
+		file=open(command,'a')
+		#list of basic tags
+		var = ['mass','Height_storey','Stiffness_storey']
+		jar=0
+		#writing matix into sage file
+		for j in var:
+			message="Less no. of rows in csv file"
+			file.write(j)
+			file.write('=matrix([')
+			#writing elements of matix
+			for i in range(int(num)):
+				file.write('[')
+				#getting input from tags
+				message="Less no. of elements in row "+str(i)
+				if(not data[jar][i].isdigit()):
+					ii=i+1
+				else:
+					ii=i
+				d=data[jar][ii]
+				print(d)
+				file.write(str(d))
+				file.write(']')
+				#condition to check last element
+				if( i!=int(num)+jar-1):
+					file.write(',')
+			jar=jar+1
+			file.write('])\n')
+		file.close()
 
 
-	#creating and writing sh file for background processing
-	command=name+'/civil.sh'
-	file=open(command,'w')
-	command='cd '+name
-	file.write(command)
-	file.write('\nlatex civil.tex\nsage civil.sagetex.sage\n')
-	file.write('pdflatex civil.tex\n')
-	file.close()
-	#calling sh file for background processing
-	command='sh '+name+'/civil.sh'
-	os.system(command)
-
-	#opening creted pdf to display to user
-	command=name+'/civil.pdf'
-	f=open(command)
-	#sending pdf as response
-	response = HttpResponse(f,content_type='application/pdf')
-	response['Content-Disposition'] = 'attachment; filename="civil.pdf"'
-	if(request.POST.get('email_id')):
-		email_id=request.POST.get('email_id')
-		user_email = EmailMessage('Your PDF is ready!',
-		'You have is ready', to=[email_id])
-		user_email.attach_file(command)
-		user_email.send()
-		command='rm -rf '+name
+		#creating and writing sh file for background processing
+		command=name+'/civil.sh'
+		file=open(command,'w')
+		command='cd '+name
+		file.write(command)
+		file.write('\nlatex civil.tex\nsage civil.sagetex.sage\n')
+		file.write('pdflatex civil.tex\n')
+		file.close()
+		#calling sh file for background processing
+		command='sh '+name+'/civil.sh'
 		os.system(command)
-		return render(request, "civilsage/index.html", {})
-	else:
-		#deleting temperary files
-		command='rm -rf '+name
-		os.system(command)
-		return response
+
+		#opening creted pdf to display to user
+		command=name+'/civil.pdf'
+		f=open(command)
+		#sending pdf as response
+		response = HttpResponse(f,content_type='application/pdf')
+		response['Content-Disposition'] = 'attachment; filename="civil.pdf"'
+		if(request.POST.get('email_id')):
+			email_id=request.POST.get('email_id')
+			user_email = EmailMessage('Your PDF is ready!',
+			'You have is ready', to=[email_id])
+			user_email.attach_file(command)
+			user_email.send()
+			command='rm -rf '+name
+			os.system(command)
+			return render(request, "civilsage/index.html", {})
+		else:
+			#deleting temperary files
+			command='rm -rf '+name
+			os.system(command)
+			return render(response)
+	except:
+		return render(request, "civilsage/file.html",
+		{'message':message,'email_get':request.session.get('email_get')})
