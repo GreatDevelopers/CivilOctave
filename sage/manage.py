9062b8a037385639bc66c9f6cf00b2d59dc18f1d
amarjeetkapoor1 <amarjeet.kapoor1@gmail.com>
2015-11-10 21:12:56 +0530

    inital script execution removed

--- sage/manage.py
+++ sage/manage.py
@@ -4,9 +4,9 @@ import sys,threading
 import initial_file
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=initial_file.pdfemail,args=())
-    thread.daemon = True
-    thread.start()
+    #thread = threading.Thread(target=initial_file.pdfemail,args=())
+    #thread.daemon = True
+    #thread.start()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sage.settings")
 
     from django.core.management import execute_from_command_line
--- sage/shell_script.sh
+++
@@ -1,3 +0,0 @@
-sage sagemath/input.sage
-
-## add code to clear intrupped requests
